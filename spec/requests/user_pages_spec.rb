93c16c3743f3acc14e08f1143084d05adda1af80
Lillie Chilen <lillie.chilen@gmail.com>
2013-03-05 16:48:51 -0800

    Add first half of user authorization

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -13,8 +13,8 @@ describe "User pages" do
 
   describe "profile page" do
   	let(:user) { FactoryGirl.create(:user)}
-  	before { visit user_path(user)}
-
+    before { visit user_path(user) }
+  	
   	it { should have_selector('h1', text: user.name)}
   	it { should have_selector('title', text: user.name)}
   end
@@ -65,6 +65,45 @@ describe "User pages" do
         it { should have_content('Welcome to the Sample App!') }
         it { should have_link('Sign out')}
       end 
+    end
+  end
+
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+    
+    describe "page" do
+      it { should have_selector('h1', text: "Update your profile") }
+      it { should have_selector('title', text: "Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
+
+    describe "with valid information" do
+      let(:new_name) { "Dog Dog" }
+      let(:new_email) { "dog@dog.dog"}
+
+      before do
+        fill_in "Name", with: new_name
+        fill_in "Email", with: new_email
+        fill_in "Password", with: user.password
+        fill_in "Confirm Password", with: user.password
+        click_button "Save changes"
+      end
+
+      it { should have_selector('title', text: new_name) }
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link("Sign out", href: signout_path) }
+      specify { user.reload.name.should == new_name }
+      specify { user.reload.email.should == new_email }
     end
   end
 end
