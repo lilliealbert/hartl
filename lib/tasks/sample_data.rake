d965d55c3fd9003b4558542e4a73b7de65f28b8b
Lillie Chilen <lillie.chilen@gmail.com>
2013-03-11 16:55:21 -0700

    Showing microposts

--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -5,7 +5,9 @@ namespace :db do
                   email: "cat@example.cat",
                   password: "password",
                   password_confirmation: "password")
+    
     admin.toggle!(:admin)
+    
     99.times do |n|
       name = Faker::Name.name
       email = "example-#{n+1}@example.cat"
@@ -14,6 +16,12 @@ namespace :db do
                     email: email,
                     password: password,
                     password_confirmation: password)
+    end
+    
+    users = User.all(limit: 6)
+    50.times do
+      content = Faker::Lorem.sentence(5)
+      users.each { |u| u.microposts.create!(content: content) }
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,6 +28,7 @@ describe User do
 	it { should respond_to(:password_confirmation) }
 	it { should respond_to(:authenticate) }
 	it { should respond_to(:remember_token) }
+	it { should respond_to(:microposts)}
 
 	describe "when name is not present" do
 		before { @user.name = " " }
@@ -141,4 +142,27 @@ describe User do
   	it { should be_admin }
   end
 
+  describe "micropost associations" do
+
+	 	before { @user.save }
+	 	let!(:older_micropost) do
+	 		FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+	 	end
+	 	let!(:newer_micropost) do
+	 		FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+	 	end
+
+	 	it "should have the right microposts in the right order" do
+	 		@user.microposts.should == [newer_micropost, older_micropost]
+	 	end
+
+	 	it "should destroy associated microposts" do
+	 		microposts = @user.microposts.dup
+	 		@user.destroy
+	 		microposts.should_not be_empty
+	 		microposts.each do |m|
+	 			Micropost.find_by_id(m.id).should be_nil
+	 		end
+	 	end
+  end
 end
